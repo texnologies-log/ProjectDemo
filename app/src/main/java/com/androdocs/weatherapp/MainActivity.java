2225195f964d0500df09228aab752c9ca93de083
Melitos <nikosmelitos@gmail.com>
2019-11-05 18:29:07 +0200

    nikos

--- app/src/main/java/com/androdocs/weatherapp/MainActivity.java
+++ app/src/main/java/com/androdocs/weatherapp/MainActivity.java
@@ -1,2 +1,45 @@
 package com.androdocs.weatherapp;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.androdocs.httprequest.HttpRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
+            sunsetTxt, windTxt, pressureTxt, humidityTxt;
+
+
+    String CITY = "serres,gr";
+    String API = "d830ac00c13e0f678ca1c3a9112a62e8";
+
+    boolean exeption = false;
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            exeption = true;
+        }
+
+
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            return response;
+        }
+    }
+}
