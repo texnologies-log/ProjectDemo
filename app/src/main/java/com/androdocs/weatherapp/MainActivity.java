402d8d084b806b2edb11c29fccf4ff8357d962a7
Savvas <savvas.sidiropoulos96@gmail.com>
2019-11-07 22:35:17 +0200

    Allagi layout, Savvas

--- app/src/main/java/com/androdocs/weatherapp/MainActivity.java
+++ app/src/main/java/com/androdocs/weatherapp/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.androdocs.weatherapp;
 
 import android.os.AsyncTask;
@@ -16,79 +17,87 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
+        String CITY = "serres,gr";
+        String API = "d830ac00c13e0f678ca1c3a9112a62e8";
 
-    TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
-            sunsetTxt, windTxt, pressureTxt, humidityTxt;
+        TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt,  windTxt, pressureTxt, humidityTxt;
 
-
-    String CITY = "serres,gr";
-    String API = "d830ac00c13e0f678ca1c3a9112a62e8";
-
-    boolean exeption = false;
-
-    class weatherTask extends AsyncTask<String, Void, String> {
         @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        protected void onPreExecute() {
-            super.onPreExecute();
-            exeption = true;
+            addressTxt = findViewById(R.id.address);
+            updated_atTxt = findViewById(R.id.updated_at);
+            statusTxt = findViewById(R.id.status);
+            tempTxt = findViewById(R.id.temp);
+            windTxt = findViewById(R.id.wind);
+            pressureTxt = findViewById(R.id.pressure);
+            humidityTxt = findViewById(R.id.humidity);
+
+            new weatherTask().execute();
         }
 
+        class weatherTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
 
-        protected String doInBackground(String... args) {
-            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
-            return response;
-        }
-        @Override
-        protected void onPostExecute(String result) {
-
-
-            try {
-                JSONObject jsonObj = new JSONObject(result);
-                JSONObject main = jsonObj.getJSONObject("main");
-                JSONObject sys = jsonObj.getJSONObject("sys");
-                JSONObject wind = jsonObj.getJSONObject("wind");
-                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
-
-                Long updatedAt = jsonObj.getLong("dt");
-                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
-                String temp = main.getString("temp") + "°C";
-                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
-                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
-                String pressure = main.getString("pressure") + " hpa";
-                String humidity = main.getString("humidity") + " %";
-
-                Long sunrise = sys.getLong("sunrise");
-                Long sunset = sys.getLong("sunset");
-                String windSpeed = wind.getString("speed") + " m/s";
-                String weatherDescription = weather.getString("description");
-
-
-
-                String address = jsonObj.getString("name") + ", " + sys.getString("country");
-
-
-
-                addressTxt.setText(address);
-                updated_atTxt.setText(updatedAtText);
-                statusTxt.setText(weatherDescription.toUpperCase());
-                tempTxt.setText(temp);
-                temp_minTxt.setText(tempMin);
-                temp_maxTxt.setText(tempMax);
-                sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
-                sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
-                windTxt.setText(windSpeed);
-                pressureTxt.setText(pressure);
-                humidityTxt.setText(humidity);
-
-
-
-            } catch (JSONException e) {
-                exeption=true;
+                /* Showing the ProgressBar, Making the main design GONE */
+                findViewById(R.id.loader).setVisibility(View.VISIBLE);
+                findViewById(R.id.mainContainer).setVisibility(View.GONE);
+                findViewById(R.id.errorText).setVisibility(View.GONE);
             }
 
+            protected String doInBackground(String... args) {
+                String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+                return response;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+
+
+                try {
+                    JSONObject jsonObj = new JSONObject(result);
+                    JSONObject main = jsonObj.getJSONObject("main");
+                    JSONObject sys = jsonObj.getJSONObject("sys");
+                    JSONObject wind = jsonObj.getJSONObject("wind");
+                    JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                    Long updatedAt = jsonObj.getLong("dt");
+                    String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                    String temp = main.getString("temp") + "°C";
+                    String pressure = main.getString("pressure") + " hpa";
+                    String humidity = main.getString("humidity") + "%";
+
+                    String windSpeed = wind.getString("speed");
+                    String weatherDescription = weather.getString("description");
+
+                    String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                    /* Populating extracted data into our views */
+                    addressTxt.setText(address);
+                    updated_atTxt.setText(updatedAtText);
+                    statusTxt.setText(weatherDescription.toUpperCase());
+                    tempTxt.setText(temp);
+                    windTxt.setText(windSpeed);
+                    pressureTxt.setText(pressure);
+                    humidityTxt.setText(humidity);
+
+                    /* Views populated, Hiding the loader, Showing the main design */
+                    findViewById(R.id.loader).setVisibility(View.GONE);
+                    findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+
+
+                } catch (JSONException e) {
+                    findViewById(R.id.loader).setVisibility(View.GONE);
+                    findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+                }
+
+            }
         }
     }
-}
